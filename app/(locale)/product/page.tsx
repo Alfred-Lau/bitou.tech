0805983c1cbf946431edb782cdc0dd7adae7b775
Alfred-Lau <jian.liu@icloud.com>
2023-10-09 23:41:40 +0800

    :sparkle: feat: add product ui layoput

--- app/(locale)/product/page.tsx
+++ app/(locale)/product/page.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
-export default function page() {
-  return <div>page</div>;
+export default function Product() {
+  return (
+    <div className="container min-h-[calc(100vh-400px)] mt-[60px]">
+      <div className="my-[60px]">
+        <ul className="grid grid-cols-12 gap-[20px]">
+          {[
+            'bitou',
+            'bitou-pen',
+            'bitou-tracer',
+            'bitou-cli',
+            'bitou-ext',
+            'bitou-ai',
+          ].map((item, index) => {
+            return (
+              <li
+                className="col-span-4 border rounded-[4px] h-[300px]"
+                key={index}
+              >
+                <div className="flex justify-center items-center h-full">
+                  {item}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
