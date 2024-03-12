9ad8ab3305646ca6446f3f6afb1a4dd71e20ba08
Alfred-Lau <jian.liu@icloud.com>
2024-03-12 11:53:26 +0800

    feat: add blog main pag

--- app/(locale)/blog/layout.tsx
+++ app/(locale)/blog/layout.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 export const metadata = {
   title: '博客 - 笔头',
   description: '博客 - 笔头',
@@ -12,22 +10,6 @@ export default function RootLayout({
 }) {
   return (
     <div className="container">
-      <div className=" flex  justify-between">
-        <div className=" h-[400px]  flex  items-start  flex-col  justify-center  w-[560px]">
-          <h3 className="  text-[40px]">最新发布</h3>
-          <p className=" mt-[20px] text-[18px] leading-[32px]">
-            你的分享值得被时光看见。关注全链路流量和内容的整个生命周期，笔头，专注个人品牌的运营工具，效率提升打造
-          </p>
-        </div>
-        <div>
-          <Image
-            src="https://bitou-tech.oss-cn-hangzhou.aliyuncs.com/uploads/20230518094635_59921cb4.png"
-            width={720}
-            height={360}
-            alt=""
-          ></Image>
-        </div>
-      </div>
       <div>{children}</div>
     </div>
   );
--- app/(locale)/blog/page.tsx
+++ app/(locale)/blog/page.tsx
@@ -2,6 +2,7 @@
 import { getLocalBlogList } from '@/app/api/blog';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import Image from 'next/image';
 
 export type BlogType = {
   title: string;
@@ -34,33 +35,51 @@ export default function Blog(props: any) {
   if (blogs.length === 0) return <div>暂无博客</div>;
 
   return (
-    <div className=" grid  grid-cols-12  gap-[40px]  my-[40px]">
-      {blogs.map((blog, index) => {
-        return (
-          blog && (
-            <Link
-              key={index}
-              href={`/blog/${blog.slug}`}
-              className="col-span-4 aspect-[1] overflow-hidden rounded-[4px] border border-gray-90  hover:border-gray-80 transition-colors duration-200 relative"
-            >
-              <img
-                className="h-[260px] min-w-full object-contain"
-                src={blog.feature_image}
-                alt={blog.title}
-                width={undefined}
-                height={undefined}
-              />
-              <div className=" p-[20px]">
-                <h2>{blog.title}</h2>
-                <div>{blog.description?.slice(0, 10)}</div>
-                <div>{blog.content?.slice(0, 21)}</div>
-                <div>{blog.published_at}</div>
-                <div>{blog.tags}</div>
-              </div>
-            </Link>
-          )
-        );
-      })}
+    <div className="">
+      <div className="flex  justify-between">
+        <div className=" h-[400px]  flex  items-start  flex-col  justify-center  w-[560px]">
+          <h3 className="  text-[40px]">最新发布</h3>
+          <p className=" mt-[20px] text-[18px] leading-[32px]">
+            你的分享值得被时光看见。关注全链路流量和内容的整个生命周期，笔头，专注个人品牌的运营工具，效率提升打造
+          </p>
+        </div>
+        <div>
+          <Image
+            src="https://bitou-tech.oss-cn-hangzhou.aliyuncs.com/uploads/20230518094635_59921cb4.png"
+            width={720}
+            height={360}
+            alt=""
+          ></Image>
+        </div>
+      </div>
+      <div className="grid  grid-cols-12  gap-[40px]  my-[40px]">
+        {blogs.map((blog, index) => {
+          return (
+            blog && (
+              <Link
+                key={index}
+                href={`/blog/${blog.slug}`}
+                className="col-span-4 aspect-[1] overflow-hidden rounded-[4px] border border-gray-90  hover:border-gray-80 transition-colors duration-200 relative"
+              >
+                <img
+                  className="h-[260px] min-w-full object-contain"
+                  src={blog.feature_image}
+                  alt={blog.title}
+                  width={undefined}
+                  height={undefined}
+                />
+                <div className=" p-[20px]">
+                  <h2>{blog.title}</h2>
+                  <div>{blog.description?.slice(0, 10)}</div>
+                  <div>{blog.content?.slice(0, 21)}</div>
+                  <div>{blog.published_at}</div>
+                  <div>{blog.tags}</div>
+                </div>
+              </Link>
+            )
+          );
+        })}
+      </div>
     </div>
   );
 }
