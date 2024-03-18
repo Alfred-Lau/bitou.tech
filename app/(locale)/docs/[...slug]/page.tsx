4a6c1244641bf72b892e2f8ddfe480e15102b682
Alfred-Lau <jian.liu@icloud.com>
2024-03-18 22:04:29 +0800

    feat: add component  css

--- app/(locale)/docs/[...slug]/page.tsx
+++ app/(locale)/docs/[...slug]/page.tsx
@@ -1,10 +1,10 @@
-import { getDocDetail } from '@/app/api/doc';
-import PostLayout from '@/app/components/docs/post-layout';
-import Content from '@/app/components/shared/content';
+import { getDocDetail } from "@/app/api/doc";
+import PostLayout from "@/app/components/docs/post-layout";
+import Content from "@/app/components/shared/content";
 
 export default function Docs({ params }: { params: { slug: string[] } }) {
   const { title, feature_image, slug, breadcrumbs, navigationLinks, content } =
-    getDocDetail(params.slug.join('/'));
+    getDocDetail(params.slug.join("/"));
 
   return (
     <article className="col-span-6 col-start-4 flex flex-col lg:col-span-9 md:col-span-full">
