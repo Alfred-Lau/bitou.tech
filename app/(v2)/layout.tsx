a26d5249cbb97298026231f3c66942eba95b7bed
榆关 <jian.liu@icloud.com>
2024-07-02 16:56:50 +0800

    Update layout.tsx

--- app/(v2)/layout.tsx
+++ app/(v2)/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
       />
       <link rel="icon" href="/favicon.png" sizes="any" />
      <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
+    (function(c:any,l:any,a:any,r:any,i:any,t?:any,y?:any){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
